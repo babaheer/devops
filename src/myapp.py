b92fa267c93aaf4a119d37e2657a7ac8f6f7e45d
babaheer <baseer.baheer@ut.ee>
2022-02-16 14:01:54 +0200

    MyApp Changed

--- src/myapp.py
+++ src/myapp.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return "Hello, Estonia!"
 
-# @app.route("/ping")
-# def pong():
-#     return "Pong"
+@app.route("/ping")
+def pong():
+    return "Pong"
 
 if __name__ == "__main__":
     app.run(debug=True)
